5a89e8a02986fb30f5567f3853e780e05f53fece
jeff890312 <96343624+jeff890312@users.noreply.github.com>
2022-08-24 22:35:28 +0800

    Add files via upload

---
+++ Utilities/NotEqualToAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ITAXI.Utilities
+{
+    public class NotEqualToAttribute : ValidationAttribute
+    {
+        private readonly string _otherPropertyDisplayName;
+
+        public NotEqualToAttribute(string otherPropertyDisplayName)
+        {
+            _otherPropertyDisplayName = otherPropertyDisplayName;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var otherProperty = validationContext.ObjectType.GetProperty(_otherPropertyDisplayName);
+            if (otherProperty == null) throw new ArgumentException("other property with the display name not found");
+            var otherPropertyValue = otherProperty.GetValue(validationContext.ObjectInstance);
+
+            if (Object.Equals(value, otherPropertyValue))
+                return new ValidationResult(ErrorMessageString);
+            else
+                return ValidationResult.Success;
+        }
+
+    }
+}
